49e6ea1c699092bbed43510bdf13555438ad1fe2
Brendan Leighton <br3ndan.l8n@gmail.com>
2024-06-27 18:25:26 -0400

    fixing gh-pages deployment 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './br3ndan-l8n.github.io/HoboWars2_UniversityMiniGame',
+	base: './',
 	plugins: [react()],
 })
